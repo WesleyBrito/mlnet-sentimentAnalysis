64faba7b97ee39212c7318f0e8173f2237db9412
Wesley Brito <wesley.brito.oliveira@gmail.com>
2025-01-10 15:09:05 +0000

    load and split data

--- SentimentAnalysisConsole/Program.cs
+++ SentimentAnalysisConsole/Program.cs
@@ -1,1 +1,24 @@
-﻿System.Console.WriteLine("Hello World!");
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using static System.Console;
+
+#region const
+const string dataFileName = "yelp_labelled.txt";
+const string folderName = "Data";
+#endregion
+
+string _dataPath = Path.Combine(Environment.CurrentDirectory, folderName, dataFileName);
+
+MLContext mLContext = new MLContext(seed: 0);
+
+TrainTestData splitDataView = LoadData(mLContext, _dataPath);
+
+TrainTestData LoadData(MLContext mLContext, string dataPath)
+{
+    IDataView dataView = mLContext.Data.LoadFromTextFile<SentimentData>(dataPath, hasHeader: false);
+    TrainTestData splitDataView = mLContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+    return splitDataView;
+}
+
+ReadLine();
---
+++ SentimentAnalysisConsole/models/SentimentPrediction .cs
@@ -0,0 +1,11 @@
+using Microsoft.ML.Data;
+
+public class SentimentPrediction : SentimentData
+{
+    [ColumnName("PredictedLabel")]
+    public bool Prediction { get; set; }
+
+    public float Probability { get; set; }
+
+    public float Score { get; set; }
+}
