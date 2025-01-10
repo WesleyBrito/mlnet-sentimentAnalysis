e9b0bcd4ce0a01df96989e401f883b53f5e95420
Wesley Brito <wesley.brito.oliveira@gmail.com>
2025-01-10 14:24:01 +0000

    Config copy file yelp_labelled.txt to output directory

--- SentimentAnalysisConsole/obj/Debug/net9.0/SentimentAnalysisConsole.AssemblyInfo.cs
+++ SentimentAnalysisConsole/obj/Debug/net9.0/SentimentAnalysisConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SentimentAnalysisConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14bcc6378a1f7ec7f901c8935c2597eb8f6a6ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e8a36fce9e771756636619a4d3558a2edc49a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SentimentAnalysisConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SentimentAnalysisConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
